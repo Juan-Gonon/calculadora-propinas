d631b652439ea3a8f2d354efa613cc7495c581da
Juan Gonon <juanfegonon27@gmail.com>
2024-06-18 12:08:00 -0600

    type props addItem

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -1,12 +1,12 @@
 import { type MenuItem } from "../types"
 type MenuItemProps = {
     item : MenuItem;
-    addItem(): void
+    addItem(item:MenuItem): void
 }
 
 export function MenuItems({item, addItem}: MenuItemProps) {
   return (
-    <button className="border-2 border-teal-200 hover:bg-teal-500 w-full p-3 flex justify-between " onClick={addItem} >
+    <button className="border-2 border-teal-200 hover:bg-teal-500 w-full p-3 flex justify-between " onClick={() => addItem(item)} >
     <p>{item.name}</p>
     <p className=" font-black "> ${item.price}</p>
     </button>
