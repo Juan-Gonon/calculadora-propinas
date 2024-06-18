5fa86df798e21305a7625c7312caa5a382b1ef68
Juan Gonon <juanfegonon27@gmail.com>
2024-06-18 11:31:29 -0600

    style menuItem and main

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,15 @@ function App() {
       <h1 className=" text-center text-4xl font-black " >Calculadora de Propinas</h1>
     </header>
     <main className=" max-w-7xl mx-auto py-20 bg-slate-700 grid md:grid-cols-2  ">
-      <div>
-        <h2>Menu</h2>
-        {
-          menuItems.map((item) => {
-            return <MenuItems key={item.id} />
-          })
-        }
+      <div className=" p-5 " >
+        <h2 className=" text-4xl font-black mb-3 " >Menu</h2>
+        <div className=" space-y-3 mt-10 " >
+          {
+            menuItems.map((item) => {
+              return <MenuItems key={item.id} item={item} />
+            })
+          }
+        </div>
       </div>
       <div>
         <h2>Consumo</h2>
--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -1,7 +1,13 @@
+import { type MenuItem } from "../types"
+type MenuItemProps = {
+    item : MenuItem
+}
 
-
-export function MenuItems() {
+export function MenuItems({item}: MenuItemProps) {
   return (
-    <div>MenuItems</div>
+    <button className="border-2 border-teal-200 hover:bg-teal-500 w-full p-3 flex justify-between " >
+    <p>{item.name}</p>
+    <p className=" font-black "> ${item.price}</p>
+    </button>
   )
 }
