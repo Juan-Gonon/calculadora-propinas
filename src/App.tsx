bdecd32b05de1d79751bb1de645f3ce9de436e52
Juan Gonon <juanfegonon27@gmail.com>
2024-06-18 15:41:32 -0600

    finished propina

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       <div className=" border border-dashed border-slate-300 p-5 rounded-lg space-y-10 ">
         <OrderContents order={order} removeItem={removeItem} />
         <TiPorcentageForm setTip={setTip} />
-        <OrderTotals order={order} />
+        <OrderTotals order={order} tip={tip} />
       </div>
    
     </main>
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -2,13 +2,18 @@ import { useMemo } from "react"
 import { OrderItem } from "../types"
 import { formatCurrency } from "../helpers/intex"
 type OrderTotalsProps = {
-    order : OrderItem[]
+    order : OrderItem[];
+    tip: number
 }
 
-export function OrderTotals({order} : OrderTotalsProps) {
+export function OrderTotals({order, tip } : OrderTotalsProps) {
     const subTotalAmount = useMemo(() =>{
         return order.reduce((total, item) => total + (item.quantity * item.price), 0 )
     }, [order])
+
+    const tipAmount = useMemo(() =>{
+        return subTotalAmount * tip
+    }, [tip, order])
 
   return (
     <>
@@ -16,7 +21,7 @@ export function OrderTotals({order} : OrderTotalsProps) {
         <h2 className=" font-black text-2xl " > Totales y Propinas:</h2>
         <p>Subtotal a pagar: <span className="font-bold" >{formatCurrency(subTotalAmount)}</span>
         </p>
-        <p>Propina: <span className="font-bold">$0</span></p>
+        <p>Propina: <span className="font-bold">{formatCurrency(tipAmount)}</span></p>
         <p>Total a Pagar: <span className="font-bold">$0</span></p>
     </div>
     </>
